d80e3183e9a13614e1e719e87a197c601d1582e8
Leonardo <leonardo.gomes@snfsistemas.com.br>
2023-05-07 12:15:18 -0300

    user on character

--- db/migrate/20230426001713_create_characters.rb
+++ db/migrate/20230426001713_create_characters.rb
@@ -3,7 +3,7 @@
 class CreateCharacters < ActiveRecord::Migration[7.0]
   def change
     create_table :characters do |t|
-      
+      t.references :user, null: false, foreign_key: true, index: true
       t.references :base_character, null: false, foreign_key: true, index: true
       
       t.string :name, null: false

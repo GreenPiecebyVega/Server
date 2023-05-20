dd8486a042c5298884b9ca121a1b5f0b23bbd50a
Leonardo <leonardo.gomes@snfsistemas.com.br>
2023-05-20 14:24:00 -0300

    retrieving some data from the user character

--- app/serializers/user_character_serializer.rb
+++ app/serializers/user_character_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class UserCharacterSerializer < ActiveModel::Serializer
-  attributes :id
+  type :user_character
+  attributes :id, :nickname, :lv
+
+  belongs_to :user
+  has_one :character
 end

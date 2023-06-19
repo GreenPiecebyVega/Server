5355e1678e1d0eff7ac389504c1d580e6df0ece1
Leonardo <leonardo.gomes@snfsistemas.com.br>
2023-06-19 17:36:55 -0300

    docker entrypoint adjustment

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ "$1" == "bundle" ] && [[ ! "$@" == *"sidekiq"* ]]; then
+if [ "$1" == "bundle" ] && [[ ! "$*" == *"sidekiq"* ]]; then
   bundle exec rake db:create 2>/dev/null
   bundle exec rake db:migrate 2>/dev/null
   bundle exec rake db:seed

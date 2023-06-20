f2e39da2489fbbf21621dc54fee78e3cda560a23
vishhnu-dev <avitassibanac@gmail.com>
2023-06-20 17:52:46 -0300

    Pid's in use fixed on docker-entrypoint

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -2,6 +2,13 @@
 set -e
 
 if [ "$1" == "bundle" ] && [[ ! "$*" == *"sidekiq"* ]]; then
+  
+  if [ -f tmp/pids/server.pid ]; then
+    rm tmp/pids/server.pid
+  elif [ -f tmp/pids/server_test.pid ]; then
+    rm tmp/pids/server_test.pid
+  fi
+  
   bundle exec rake db:create 2>/dev/null
   bundle exec rake db:migrate 2>/dev/null
   bundle exec rake db:seed
